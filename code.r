b9198678fb9acb4ea9e7ca3dbcc97a8d3d80b7a7
tianyu liu <hulan1991@gmail.com>
2016-08-22 23:43:56 -0400

    Update code.r

--- code.r
+++ code.r
@@ -8,12 +8,10 @@ cost<-read.csv("cost.csv",header=TRUE,stringsAsFactors=FALSE,colClasses=classes)
 
 colnames(cost) <-c("Date","Open","High","Low","Close","Volumn")
 cost$Date<-as.Date(cost$Date,format="%Y-%m-%d")
-cost$Average<-rowMeans(subset(cost, select = c(High, Low)), na.rm = TRUE)
-Volumn<-cost$Volumn
 
 cost <-cost[order(cost$Date),] 
 #plot
-png(file="HistoricalPrices5y.png",width=680,height=480)
+png(file="HistoricalPrices5y.png",width=580,height=480)
 ggplot(cost, aes(cost$Date,cost$Close))+
 geom_line(colour="#ffa07a")+
 ggtitle("Historical Prices for COST Stock in Five Year")+
@@ -21,9 +19,48 @@ xlab("Date")+ylab("price($)")
 dev.off()
 
 cost1y<-cost[1005:1256,]
-png(file="HistoricalPrices1y.png",width=680,height=480)
+png(file="HistoricalPrices1y.png",width=580,height=480)
 ggplot(cost[1005:1256,], aes(cost$Date[1005:1256],cost$Close[1005:1256]))+
 geom_line(colour="#ffa07a")+
 ggtitle("Historical Prices for COST Stock in One Year")+
 xlab("Date")+ylab("price($)")
 dev.off()
+
+
+p<-ggplot(cost, aes(x=cost$Date,y=cost$Close))+
+geom_line(colour="#ffa07a")+
+geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x)+
+ggtitle("Linear Regression Model")+
+xlab("Date")+ylab("price($)")
+
+
+lm_eqn <- function(df){
+    m <- lm(Close ~ Date, cost);
+    eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
+         list(a = format(coef(m)[1], digits = 2), 
+              b = format(coef(m)[2], digits = 2), 
+             r2 = format(summary(m)$r.squared, digits = 3)))
+    as.character(as.expression(eq));                 
+}
+
+png(file="LinearRegressionModel.png",width=580,height=480)
+p + geom_text(x = 15670, y = 150, label = lm_eqn(df), parse = TRUE)
+dev.off()
+
+summary(m)
+
+##difference log model
+dif<-diff(log(cost$Close))
+png(file="HistogramDifferenceLog(Closing Price).png",width=580,height=480)
+qplot(dif,xlab="Difference",ylab="Count",main="Histogram of Difference in Log(Close Price)",fill=I("#ffa07a"),binwidth=0.003)
+dev.off()
+
+png(file="DifferenceLog(Closing Price).png",width=580,height=480)
+ggplot(cost[-1,], aes(x=cost$Date[-1],y=dif))+
+geom_line(colour="#ffa07a")+
+ggtitle("Difference in Log(Close Price)")+
+xlab("Date")+ylab("Difference")
+dev.off()
+
+ks.test(dif,"pnorm",mean(dif),sd(dif))
+shapiro.test(dif)
