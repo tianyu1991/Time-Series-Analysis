59b6010fea2546f2904429cb2197ec2f8b0b64b3
tianyu liu <hulan1991@gmail.com>
2016-08-24 00:27:19 -0400

    Update code.r

--- code.r
+++ code.r
@@ -1,18 +1,3 @@
-library(ggplot2)
-library(datasets)
-library(dplyr)
-
-initial<-read.csv("cost.csv",nrows=100,header=TRUE,stringsAsFactors=FALSE)
-classes<-sapply(initial,class)
-cost<-read.csv("cost.csv",header=TRUE,stringsAsFactors=FALSE,colClasses=classes)
-
-colnames(cost) <-c("Date","Open","High","Low","Close","Volumn")
-cost$Date<-as.Date(cost$Date,format="%Y-%m-%d")
-
-cost <-cost[order(cost$Date),] 
-#plot
-png(file="HistoricalPrices5y.png",width=580,height=480)
-ggplot(cost, aes(cost$Date,cost$Close))+
 geom_line(colour="#ffa07a")+
 ggtitle("Historical Prices for COST Stock in Five Year")+
 xlab("Date")+ylab("price($)")
@@ -64,3 +49,35 @@ dev.off()
 
 ks.test(dif,"pnorm",mean(dif),sd(dif))
 shapiro.test(dif)
+
+diff2<-ts(dif)
+ar(dif)
+
+Close2<-ts(cost$Close,freq=365)
+
+plot(stl(log(Close2),s.window="periodic"))
+
+library(astsa)
+lag1.plot(diff2,1)
+
+
+Date2<-as.Date(cost$Date,format="%Y-%m")
+cost$Date2<-format(cost$Date,"%Y-%m")
+
+data2<-group_by(cost,Date2)
+data3<-summarise(data2,mean_Close=mean(Close))
+data4<-data.frame(data3)
+Date3<-as.Date(data4$Date2.formar="%Y-%m")
+
+ggplot(data4, aes(Date2,mean_Close))+
+geom_point(colour="#ffa07a")
+
+x<-diff(data4$mean_Close)
+xlag6=lag(x,6)
+y=cbind(x,xlag6)
+ar1fit=lm(y[,1]~y[,2])
+
+
+beer<-ts(data4$mean_Close,start=2011,freq=12)
+plot(stl(log(beer),s.window="periodic"))
+x2<-diff(data4$mean_Close)
